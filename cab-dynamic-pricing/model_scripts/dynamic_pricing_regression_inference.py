5261e8b7d7e61d1b3824eac084543d9a4c4572ac
Shubha Changappa Palachanda <50392431+shubha8196@users.noreply.github.com>
2022-03-04 07:34:03 -0800

    Adding dynamic_pricing_regression_inference.py
    Adding dynamic_pricing_regression_inference.py script

---
+++ cab-dynamic-pricing/model_scripts/dynamic_pricing_regression_inference.py
@@ -0,0 +1,24 @@
+class CabPricePredictor:
+    def __init__(self, data_frame):
+        ''''''
+        
+        ''''''
+        self.data_frame = data_frame
+   
+    def get_uber_price(self):
+        ''''''
+        
+        ''''''
+        filename = "../model_weights/uber_mlr_model"
+        uber_mlr_model = pickle.load(open(filename, 'rb'))
+        uber_price = loaded_model.predict(self.data_frame)
+        return uber_price
+        
+    def get_lyft_price(self):
+        ''''''
+        
+        ''''''
+        filename = "../model_weights/lyft_mlr_model"
+        lyft_mlr_model = pickle.load(open(filename, 'rb'))
+        lyft_price = loaded_model.predict(self.data_frame)
+        return lyft_price
