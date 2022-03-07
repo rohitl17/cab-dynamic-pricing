14ce6e5f7a7fc319a1a87f7f0548288daa9c0bfe
Shubha Changappa Palachanda <50392431+shubha8196@users.noreply.github.com>
2022-03-06 20:42:09 -0800

    Update dynamic_pricing_model_training.py

---
+++ cab-dynamic-pricing/feedback_app/dynamic_pricing_model_training.py
@@ -0,0 +1,84 @@
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from datetime import datetime
+from pathlib import Path  
+import pickle 
+import pandas as pd
+
+class PricePredictModel:
+    def __init__(self):
+        
+        """
+        
+
+        
+        """
+        
+        self.uber_train = pd.read_csv('./training_testing_data/uber_train_mlr.csv')
+        self.lyft_train = pd.read_csv('./training_testing_data/lyft_train_mlr.csv')
+        
+        self.uber_test = pd.read_csv('./training_testing_data/uber_test_mlr.csv')
+        self.lyft_test = pd.read_csv('./training_testing_data/lyft_test_mlr.csv')
+        
+        Xuber_train = self.uber_train[['source_lat','source_long', 'dest_lat', 'dest_long','distance','surge_multiplier','Black','Black SUV', 'UberPool', 'UberX', 'UberXL', 'WAV']]
+        Yuber_train = self.uber_train['price']
+        
+        Xuber_test = self.uber_test[['source_lat','source_long', 'dest_lat', 'dest_long','distance','surge_multiplier','Black','Black SUV', 'UberPool', 'UberX', 'UberXL', 'WAV']]
+        Yuber_test = self.uber_test['price']
+        
+        Xlyft_train = self.lyft_train[['source_lat','source_long', 'dest_lat', 'dest_long','distance','surge_multiplier','Lux','Lux Black', 'Lux Black XL', 'Lyft', 'Lyft XL', 'Shared']]
+        Ylyft_train = self.lyft_train['price']
+        
+        Xlyft_test = self.lyft_test[['source_lat','source_long', 'dest_lat', 'dest_long','distance','surge_multiplier','Lux','Lux Black', 'Lux Black XL', 'Lyft', 'Lyft XL', 'Shared']]
+        Ylyft_test = self.lyft_test['price']
+        
+        self.result_uber = pd.DataFrame
+        self.result_lyft = pd.DataFrame
+        
+    def uber_train_test(self):
+        
+        """
+
+
+        """
+        
+        uber_model = linear_model.LinearRegression()
+        uber_model.fit(Xuber_train, Yuber_train)
+        
+        timestamp=datetime.now()
+        result_uber['timestamp'] = timestamp
+        
+        filename = ('./model_dumps/uber_mlr_model' + timestamp)
+        pickle.dump(uber_model,open(filename,'wb'))
+        
+        Yuber_pred = uber_model.predict(Xuber_test)  
+        
+        rsquared_value= r2_score(Yuber_test, Yuber_pred)
+        result_uber['R^2 score'] =rsquared_value
+        
+        result_uber.tocsv('./result/uber_price_mlr' + timestamp + '.csv')
+        
+    
+    def lyft_train_test(self):
+        
+        """
+
+
+        """
+        
+        lyft_model = linear_model.LinearRegression()
+        lyft_model.fit(Xlyft_train, Ylyft_train)
+        
+        timestamp=datetime.now()
+        result_lyft['timestamp'] = timestamp
+        
+        filename = ('./model_dumps/lyft_mlr_model' + timestamp)
+        pickle.dump(lyft_model,open(filename,'wb'))
+        
+        Ylyft_pred = lyft_model.predict(Xlyft_test)  
+        
+        rsquared_value= r2_score(Ylyft_test, Ylyft_pred)
+        result_lyft['R^2 score'] =rsquared_value
+        
+        result_lyft.tocsv('./result/lyft_price_mlr' + timestamp + '.csv')
+        
