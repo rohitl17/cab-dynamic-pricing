cac4cccb03584ee8074d50ece3387fa99ff33ed7
Shubha Changappa Palachanda <50392431+shubha8196@users.noreply.github.com>
2022-03-08 13:33:59 -0800

    updated tests for regression inference model

--- cab-dynamic-pricing/tests/test_dynamic_pricing_regression_inference.py
+++ cab-dynamic-pricing/tests/test_dynamic_pricing_regression_inference.py
@@ -5,18 +5,20 @@ import pandas as pd
 
 class CabPricePredictor(unittest.TestCase):
 
-    def test_get_uber_price(self):
-        uber = pd.read_csv('../feedback_app/training_testing_data/uber_test_mlr.csv')
+    def test_check_price(self):
+    	uber = pd.DataFrame({'source_lat':[42.3600825],'source_long':[-71.0588801],'dest_lat':[42.3518662],'dest_long':[-71.0642623],
+    	'distance':[1.0],'surge_multiplier':[1.0],'Black,Black SUV':[0],'UberPool':[1.0],'UberX':[0],'UberXL':[0],'WAV':[0],'uber_price':[9.0]})
 
-        message = "Price output is not created"
-        price=uber[['uber_price']]
-        message = "ERROR : test_get_uber_price is failing"
-        self.assertIsNotNone(price,message)
+    	message = "NaN values present in price"
+    	self.assertIsNotNone(uber['uber_price'], message)   
+
 
     def test_check_input(self):
-        lyft = pd.read_csv('../feedback_app/training_testing_data/lyft_test_mlr.csv')
+    	lyft = pd.DataFrame({'source_lat':[42.3600825],'source_long':[-71.0588801],'dest_lat':[42.3518662],'dest_long':[-71.0642623],
+    	'distance':[1.0],'surge_multiplier':[1.0],'Lux':[1.0],'Lux Black':[0],'Lux Black XL':[0],'Lyft':[0],'Lyft XL':[0],'Shared':[0],
+    	'lyft_price':[7.0]})
 
-        message = "NaN values present in distance"
-        self.assertIsNotNone(lyft['distance'], message)    
+    	message = "NaN values present in distance"
+    	self.assertIsNotNone(lyft['distance'], message)    
 
 unittest.main()
