a624422f0917612cc33287c0ea5c12e4f1bc50a2
Shubha Changappa Palachanda <50392431+shubha8196@users.noreply.github.com>
2022-03-07 21:52:50 -0800

    updating tests for regression inference module

--- cab-dynamic-pricing/tests/test_dynamic_pricing_regression_inference.py
+++ cab-dynamic-pricing/tests/test_dynamic_pricing_regression_inference.py
@@ -6,13 +6,13 @@ import dynamic_pricing_regression_inference as dpri
 class CabPricePredictor(unittest.TestCase):
 
     def test_get_uber_price(self):
-        uber = pd.read_csv('./training_testing_data/uber_test_mlr.csv')
+        uber = pd.read_csv('../feedback_app/training_testing_data/uber_test_mlr.csv')
 
         message = "Price output is not created"
         self.assertNotEqual(uber['price'].shape, 0, message)
 
     def test_check_input(self):
-        lyft = pd.read_csv('./training_testing_data/lyft_test_mlr.csv')
+        lyft = pd.read_csv('../feedback_app/training_testing_data/lyft_test_mlr.csv')
 
         message = "NaN values present in distance"
         self.assertIsNotNone(lyft['distance'], message)    
