6c8fa7fdde4c59caae305955e0653fefda39345d
Shubha Changappa Palachanda <50392431+shubha8196@users.noreply.github.com>
2022-03-08 15:31:13 -0800

    updated tests for dynamic_pricing_model_training

--- cab-dynamic-pricing/tests/test_dynamic_pricing_model_training.py
+++ cab-dynamic-pricing/tests/test_dynamic_pricing_model_training.py
@@ -12,7 +12,7 @@ class TestPricePredictModel(unittest.TestCase):
 
 	def test_lyft_traindata(self):
 		
-    lyft_train = pd.DataFrame({'source_lat':[42.3600825],'source_long':[-71.0588801],'dest_lat':[42.3518662],'dest_long':[-71.0642623],'distance':[1.0],'surge_multiplier':[1.0],'Lux':[1.0],'Lux Black':[0],'Lux Black XL':[0],'Lyft':[0],'Lyft XL':[0],'Shared':[0],'lyft_price':[7.0]})
+		lyft_train = pd.DataFrame({'source_lat':[42.3600825],'source_long':[-71.0588801],'dest_lat':[42.3518662],'dest_long':[-71.0642623],'distance':[1.0],'surge_multiplier':[1.0],'Lux':[1.0],'Lux Black':[0],'Lux Black XL':[0],'Lyft':[0],'Lyft XL':[0],'Shared':[0],'lyft_price':[7.0]})
 		self.assertEqual(len(lyft_train.columns),13)
 
 	def test_uber_testdata(self):	
