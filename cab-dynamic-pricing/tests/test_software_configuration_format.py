5d9ed1e7ecad5c415479910ad936fc1e514e3de2
Rohit Lokwani <rohitlokwani17@gmail.com>
2022-03-07 20:44:38 -0800

    Resolving merge conflict

---
+++ cab-dynamic-pricing/tests/test_software_configuration_format.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestSoftwareConfiguration(unittest.TestCase):
+    def test_software_configuration(self):
+        '''
+        Placeholder for testing the configuration format file
+
+        Returns
+        -------
+        None.
+
+        '''
+        self.assertEqual(0,0)
+        
+unittest.main()
---
+++ cab-dynamic-pricing/tests/test_user.py
@@ -0,0 +1,18 @@
+import unittest
+import pandas as pd
+import sys
+
+
+class TestApp(unittest.TestCase):
+
+    def test_training_usercsv(self):
+        data=pd.read_csv('../database/users.csv')
+        self.assertEqual(len(data.columns),3)
+        self.assertEqual(type(data['user_id'][0]), str)
+        self.assertIsNotNone(data)
+        self.assertEqual(len(data.dropna()), len(data))
+    
+
+
+unittest.main()
+
