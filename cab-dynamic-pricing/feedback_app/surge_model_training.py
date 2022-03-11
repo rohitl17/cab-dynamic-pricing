b6472cb97490dc912eb83c30f763326121fdc7cf
Nayantara Mohan <92545485+nayantaramohan@users.noreply.github.com>
2022-03-11 14:02:03 -0800

    Resolving flake8 errors

--- cab-dynamic-pricing/feedback_app/surge_model_training.py
+++ cab-dynamic-pricing/feedback_app/surge_model_training.py
@@ -1,49 +1,45 @@
-import numpy as np
 import pandas as pd
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
 from datetime import datetime
-from pathlib import Path  
+
 
 class SurgePriceModel:
     def __init__(self):
-        """
+        '''
         Loading the training dataset for the Surge Price Model and
         dividing it into features and target variables.
-        """
-        self.training_dataframe=pd.read_csv('./training_testing_data/\
-                                training_surge_price_classifier_df.csv')
-        self.testing_dataframe=pd.read_csv('./training_testing_data/\
-                                testing_surge_price_classifier_df.csv')
-        
-        self.feature_train=training_dataframe[['temp','clouds','pressure','rain',
-        'humidity','wind', 'rush_hr', 'date_day', 'location_latitude','location_longitude']]
-        
-        self.target_train=training_dataframe[['surge_mult']]
-        self.feature_test=testing_dataframe[['temp','clouds','pressure','rain','humidity',
-        'wind','rush_hr', 'date_day', 'location_latitude','location_longitude']]
-        
-        self.result_rf_model=pd.DataFrame()
-        
-    def train_surge_classifier(self, feature_train, target_train, feature_test, result_rf_model):
-        """
+        '''
+        self.training_dataframe = pd.read_csv('./training_testing_data/\
+        training_surge_price_classifier_df.csv')
+        self.testing_dataframe = pd.read_csv('./training_testing_data/\
+        testing_surge_price_classifier_df.csv')
+        self.feature_train = self.training_dataframe[['temp', 'clouds',
+                                                      'pressure', 'rain',
+                                                      'humidity', 'wind', 'rush_hr',
+                                                      'date_day', 'location_latitude',
+                                                      'location_longitude']]
+        self.target_train = self.training_dataframe[['surge_mult']]
+        self.feature_test = self.testing_dataframe[['temp', 'clouds', 'pressure', 
+                                                    'rain', 'humidity', 'wind', 'rush_hr',
+                                                    'date_day', 'location_latitude', 'location_longitude']]
+        self.target_test = self.testing_dataframe[['surge_mult']]
+        self.result_rf_model = pd.DataFrame()
+
+    def train_surge_classifier(self, feature_train, target_train, feature_test,
+                               target_test, result_rf_model):
+        '''
         Running the feature and target variables in a random forest
-        classifier and appending the timestamp on the output.
-        """
-        rf=RandomForestClassifier(n_estimators = 10, class_weight ='balanced')
+        classifier and appending the timestamp on the output
+        '''
+        rf = RandomForestClassifier(n_estimators = 10, class_weight = 'balanced')
         rf.fit(feature_train, target_train)
-        
-        #getting time stamp to append it into our model
-        timestamp=datetime.now()
+        timestamp = datetime.now()
         result_rf_model['timestamp'] = timestamp
-        
-        filename=('./model_dumps/surge_classification_rf_model' + timestamp)
-        pickle.dump(rf, open(filename,'wb'))
-        loaded_model=pickle.load(open(filename, 'rb'))
-        result=loaded_model.score(feature_test, target_test)
-        result_rf_model['model_score']=result
-        
-        #saving the result_rf_model dataframe into results
-        result_rf_model.to_csv('./result/surge_classification_rf_model' + timestamp + '.csv')
-        
+        filename = ('./model_dumps/surge_classification_rf_model'+timestamp)
+        pickle.dump(rf, open(filename, 'wb'))
+        loaded_model = pickle.load(open(filename, 'rb'))
+        result = loaded_model.score(feature_test, self.target_test)
+        result_rf_model['model_score'] = result
+        result_rf_model.to_csv('./result/surge_classification_rf_model'+timestamp+'.csv')
