5494d934e4f7b3b283f6b0246dcc59687ceb23f5
Nayantara Mohan <92545485+nayantaramohan@users.noreply.github.com>
2022-03-11 16:55:15 -0800

    Resolved flake8 bugs.

--- cab-dynamic-pricing/tests/test_surge_classification_inference.py
+++ cab-dynamic-pricing/tests/test_surge_classification_inference.py
@@ -1,30 +1,43 @@
 import unittest
-import pandas as pd 
+import pandas as pd
 import numpy as np
+
 
 class TestSurgePriceClassifier(unittest.TestCase):
 
     def test_get_rush_hour(self):
-        data= pd.DataFrame({'temp':[40.67],'clouds':[0.94],'pressure':[1013.76],
-        'rain':[0.0],'humidity':[0.92],'wind':[2.92],'rush_hr':[0],
-        'location_latitude':[42.3559219],'location_longitude':[-71.0549768],
-        'surge_mult':[0]})
-        
+        data = \
+            pd.DataFrame({'temp': [40.67], 'clouds': [0.94],
+                          'pressure': [1013.76], 'rain': [0.0],
+                          'humidity': [0.92], 'wind': [2.92],
+                          'rush_hr': [0],
+                          'location_latitude': [42.3559219],
+                          'location_longitude': [-71.0549768],
+                          'surge_mult': [0]})
         rush_hr = data[['rush_hr']]
         message = "ERROR : test_get_rush_hour is failing"
-        self.assertIsNotNone(rush_hr,message)
-    
+        self.assertIsNotNone(rush_hr, message)
+
     def test_unique_values(self):
-         
-        data= pd.DataFrame({'temp':[40.67],'clouds':[0.94],'pressure':[1013.76],
-        'rain':[0.0],'humidity':[0.92],'wind':[2.92],'rush_hr':[0],
-        'location_latitude':[42.3559219],'location_longitude':[-71.0549768],'surge_mult':[0]})
+        data = \
+            pd.DataFrame({'temp': [40.67], 'clouds': [0.94],
+                          'pressure': [1013.76], 'rain': [0.0],
+                          'humidity': [0.92], 'wind': [2.92],
+                          'rush_hr': [0],
+                          'location_latitude': [42.3559219],
+                          'location_longitude': [-71.0549768],
+                          'surge_mult': [0]})
         message = "ERROR : test_unique_values is failing"
-        self.assertEqual(len(list(np.unique(data['surge_mult']))), 1, message)
-    
+        self.assertEqual(len(list(np.unique(data['surge_mult']))),
+                         1, message)
+
     def test_null_values(self):
-        
-        data= pd.DataFrame({'temp':[40.67],'clouds':[0.94],'pressure':[1013.76],
-        'rain':[0.0],'humidity':[0.92],'wind':[2.92],'rush_hr':[0],
-        'location_latitude':[42.3559219],'location_longitude':[-71.0549768],'surge_mult':[0]})
-        self.assertEqual(len(data), len(data.dropna()))             
+        data = \
+            pd.DataFrame({'temp': [40.67], 'clouds': [0.94],
+                          'pressure': [1013.76], 'rain': [0.0],
+                          'humidity': [0.92], 'wind': [2.92],
+                          'rush_hr': [0],
+                          'location_latitude': [42.3559219],
+                          'location_longitude': [-71.0549768],
+                          'surge_mult': [0]})
+        self.assertEqual(len(data), len(data.dropna()))
