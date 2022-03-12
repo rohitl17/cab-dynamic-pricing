1a3b8194ea886606d57666937653aa9d75a8d0f1
Nayantara Mohan <92545485+nayantaramohan@users.noreply.github.com>
2022-03-11 16:42:52 -0800

    Resolved flake8 bugs.

--- cab-dynamic-pricing/configuration_files/software_configuration_format.py
+++ cab-dynamic-pricing/configuration_files/software_configuration_format.py
@@ -3,27 +3,23 @@
 '''
 Main Configuration
 '''
-user={"session_timeout":5}
+user = {"session_timeout": 5}
 
 '''
 In-use API credentials
 '''
-google_oauth_credentials={"secret_key":"", "google_client_id":'', 
-                          "google_client_secret":''}
-
-weather_api_credentials={"api_key":""}
-
-google_maps_api={"api_key":""}
-
+google_oauth_credentials = {"secret_key": "",
+                            "google_client_id": '',
+                            "google_client_secret": ''}
+weather_api_credentials = {"api_key": ""}
+google_maps_api = {"api_key": ""}
 
 '''
-Placeholder for database and uber and lyft API credentials 
+Placeholder for database and uber and lyft API credentials
 '''
-database_credentials={}
+database_credentials = {}
+lyft_api = {"server_access_token": "", "client_id": "",
+            "client_secret": ""}
 
-lyft_api={"server_access_token":"", "client_id":"",
-            "client_secret":""}
-
-uber_api = {"server_access_token":"", "client_id":"",
-    "client_secret":""}
-
+uber_api = {"server_access_token": "", "client_id": "",
+            "client_secret": ""}
